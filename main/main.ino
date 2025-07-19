bf537c49a8f675ec8d92123a7d32f3d57b6600b3
Gram 17 <banana99ou@gmail.com>
2025-07-19 23:24:05 +0900

    confirmed working with pin D13.

---
+++ main/main.ino
@@ -0,0 +1,52 @@
+#include <ESP32Servo.h>
+
+int pos = 0;
+
+Servo servo0;
+Servo servo1;
+Servo servo2;
+Servo servo3;
+Servo servo4;
+Servo servo5;
+
+Servo servo6;
+Servo servo7;
+Servo servo8;
+Servo servo9;
+
+
+void setup() {
+    Serial.begin(115200);
+
+    // analogWriteFreq(500);
+
+    servo0.attach(4,  0, 2000);
+    servo1.attach(13, 0, 2000);
+    servo2.attach(16, 0, 2000);
+    servo3.attach(17, 0, 2000);
+    servo4.attach(18, 0, 2000);
+    servo5.attach(19, 0, 2000);
+
+    servo6.attach(20, 0, 2000);
+    servo7.attach(21, 0, 2000);
+    servo8.attach(22, 0, 2000);
+    servo9.attach(23, 0, 2000);
+
+    Serial.println("boot");
+}
+
+void loop() {
+  // servo0.write(0);
+  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees in steps of 1 degree
+    // analogWrite(4, pos);
+		servo1.write(pos);    // tell servo to go to position in variable 'pos'
+		delay(15);             // waits 15ms for the servo to reach the position
+    Serial.println(pos);
+	}
+	for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+    // analogWrite(4, pos);
+		servo1.write(pos);    // tell servo to go to position in variable 'pos'
+		delay(15);             // waits 15ms for the servo to reach the position
+    Serial.println(pos);
+	}
+}
